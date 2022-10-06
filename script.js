19bf2d2ca6104d5078d01f0b8d5bdeb8d087d25a
bsstation <115123063+Bsstation@users.noreply.github.com>
2022-10-05 23:57:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function alteraConteudo(index = 0){
+    for(var i = 0; i < 10; i++){
+        var content = document.querySelector('#content' + i);
+        if(index == i){
+            content.style.display = 'block';
+        }else{
+            content.style.display = 'none';
+        }
+    }
+}
