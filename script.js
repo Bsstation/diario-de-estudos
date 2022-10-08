3beed87fa31db20de9f94e30a8e54b80cf4ef089
bsstation <115123063+Bsstation@users.noreply.github.com>
2022-10-07 22:58:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function alteraConteudo(index = 0){
-    for(var i = 0; i < 10; i++){
+    for(var i = 0; i <= 10; i++){
         var content = document.querySelector('#content' + i);
         if(index == i){
             content.style.display = 'block';
